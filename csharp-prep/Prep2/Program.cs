eb72e59ec15363c0deba26574b64d0cdc80df958
Emilio Sosa <emiliossosa85@gmail.com>
2023-01-07 23:57:34 -0400

    Complete prep 2 Core Requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your percentage grade? ");
+        string string_grade = Console.ReadLine();
+        int grade = int.Parse(string_grade);
+
+
+        string letter;
+   
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 || grade <= 70)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 || grade <= 60)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 || grade <= 50)
+        {
+            letter = "D";
+        }      
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >= 70) 
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        } else 
+        {
+            Console.WriteLine("You didn't pass the course.");
+        }
     }
 }
