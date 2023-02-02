d377bc1e156eac66f525934396c512ae75a723df
Emilio Sosa <emiliossosa85@gmail.com>
2023-02-02 00:02:04 -0400

    Create Fraction class

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,58 @@
+public class Fraction
+{   //Attributes
+    private int _top;
+    private int _bottom;
+
+    // Constructor
+    public Fraction()
+    {
+        this._bottom = 1;
+        this._bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        this._top = wholeNumber;
+        this._bottom = 1;
+    
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        this._top = top;
+        this._bottom = bottom;
+    }
+
+    // Settler and Getter
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        this._top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        this._bottom = bottom;
+    }
+
+    /*
+    public string GetFractionString()
+    {
+        
+    }
+
+    public double GetDecimalValue()
+    {
+
+    }
+    */
+}
