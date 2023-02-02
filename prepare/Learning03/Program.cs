7a3862b6955f8d2962e163206d6a8172d0acbead
Emilio Sosa <emiliossosa85@gmail.com>
2023-02-02 00:02:41 -0400

    Test the Franction Constructors

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+
+
+        Console.WriteLine($"Test: {f1}, Test 2: {f2}, Test 4: {f3}");
     }
 }
