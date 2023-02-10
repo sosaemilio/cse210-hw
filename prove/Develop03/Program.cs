ba8c6ba53e6841f515a73e932ca4838c8aaf7e93
Emilio Sosa <emiliossosa85@gmail.com>
2023-02-10 00:21:37 -0400

    Remove test line

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,8 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Reference reference = new Reference("John", "3", "16");
-        Console.WriteLine(reference.GetReference());
-
         string example = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.";
         Scripture scripture = new Scripture();
         scripture.SetScripture(reference, example);
