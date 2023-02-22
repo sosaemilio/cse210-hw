7eb096afb3f1f891db39db623dfe1ce232451b2c
Emilio Sosa <emiliossosa85@gmail.com>
2023-02-22 00:28:00 -0400

    Test function

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        BreathingActivity breathing = new BreathingActivity();
+        breathing.StartActivity();
     }
 }
