2c9e10e371bec1365b244dc2959936c62a567a92
Emilio Sosa <emiliossosa85@gmail.com>
2023-02-22 00:17:27 -0400

    Create activity class

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,43 @@
+public class Activity 
+{
+    // Attributes
+    private int _duration;
+    private string _activityName;
+    private string _description;
+    protected List<string> _randomPrompts = new List<string> {};
+
+    //Constructor
+    public Activity()
+    {
+
+    }
+
+    //Methods
+    public void DisplayActivity()
+    {
+        Console.WriteLine($"Welcome to the {_activityName}");
+        Console.WriteLine(_description);
+    }
+    public void SetActivity(string activityName, string description)
+    {
+        this._activityName = activityName;
+        this._description = description;
+    }
+    public void SetDuration(int seconds)
+    {
+        this._duration = seconds;
+    }
+    public int GetDuration()
+    {
+        return _duration;
+    }
+    public void SetPromptList(List<string> randomPrompts)
+    {
+        this._randomPrompts = randomPrompts;
+    }
+    public string GetCloseMessage()
+    {
+        string closeMessage = "You have completed another " + _duration + "of the " + _activityName;
+        return closeMessage;
+    }
+}   
