19e3a832d3371a397057c4c3150ed873a90d45a9
Emilio Sosa <emiliossosa85@gmail.com>
2023-02-23 20:49:21 -0400

    Unit test

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        BreathingActivity breathing = new BreathingActivity();
-        breathing.StartActivity();
+        //BreathingActivity breathing = new BreathingActivity();
+        //breathing.StartActivity();
+
+        ReflectionActivity reflection = new ReflectionActivity();
+        reflection.StartActivity();
     }
 }
